3d8f481bdd13e53dfe76e7c83f6428c592fbd689
RayFlaherty <90649331+RayFlaherty@users.noreply.github.com>
2022-03-02 13:36:57 -0600

    building models

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -7,6 +7,20 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type : DataTypes.STRING,
+      allowNull:false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id:{
+      type:DataTypes.INTEGER,
+      //reference the product model's id
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      //referene the tag model's id
+    }
   },
   {
     sequelize,
